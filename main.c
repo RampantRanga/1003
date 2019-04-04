a817278c38850287b6a86e5e93ee40bea93391d6
Lachlan Warham <c3328626@uon.edu.au>
2019-04-04 05:56:40 +0000

    lab 6 final commit

--- main.c
+++ main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
 int main() {
-    printf("testing");
-    return 0;
+    char input[] = "Encrypt this!"; //array
+    int i = 0;
+    int n = sizeof(input)/sizeof(char); //provides an endpoint where the code stops encrypting the message
+    //char output[n];
+    int k = 4; //k is the key value that will decide how many characters the rotation cipher will rotate
+    
+        for(i = 0; i < n; i++)  {
+        
+            if((input[i] > 97) && (input[i] < 122)) {
+                input[i] = input[i] - 32;
+            }
+        
+        input[i]+=k;
+        printf("%c", input[i]);
+    }
+
+return 0;
+
 }
