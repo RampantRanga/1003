e08b61f19c6a17e6acca9fc0a73f7388805ae5f2
Lachlan Warham <c3328626@uon.edu.au>
2019-04-10 01:43:26 +0000

    rotation works without modulus, need to do spaces and punctuation characters as well as numbers

--- main.c
+++ main.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
 
 int main() {
-    char input[] = "Encrypt this!"; //array
+    char input[] = "abcdefghijklmnopqrstuvwxyz"; //array
     int i = 0;
     int n = sizeof(input)/sizeof(char); //provides an endpoint where the code stops encrypting the message
     //char output[n];
-    int k = 4; //k is the key value that will decide how many characters the rotation cipher will rotate
+    int k = 6; //k is the key value that will decide how many characters the rotation cipher will rotate
     
         for(i = 0; i < n; i++)  {
         
-            if((input[i] > 97) && (input[i] < 122)) {
+            if((input[i] >= 97) && (input[i] <= 122)) {
                 input[i] = input[i] - 32;
-            }
+            }        
         
         input[i]+=k;
-        printf("%c", input[i]);
+            if(input[i] > 90) {
+                input[i] = input[i] - 26;
+            }
+
+            
+        //input[i] = ((input[i] + k) % 90) + 65;
+        printf("%c ", input[i]);
     }
 
 return 0;
