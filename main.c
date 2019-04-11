d47af29da6d06f2d6346fe6c712268a18acd250f
Lachlan Warham <c3328626@uon.edu.au>
2019-04-11 04:20:34 +0000

    Spaces work now, need to do numbers and symbols

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    char input[] = "abcdefghijklmnopqrstuvwxyz"; //array
+    char input[] = "Encrypt tHis!"; //array
     int i = 0;
     int n = sizeof(input)/sizeof(char); //provides an endpoint where the code stops encrypting the message
     //char output[n];
-    int k = 6; //k is the key value that will decide how many characters the rotation cipher will rotate
+    int k = 4; //k is the key value that will decide how many characters the rotation cipher will rotate
     
         for(i = 0; i < n; i++)  {
         
@@ -16,11 +16,14 @@ int main() {
         input[i]+=k;
             if(input[i] > 90) {
                 input[i] = input[i] - 26;
-            }
+            }   
 
             
-        //input[i] = ((input[i] + k) % 90) + 65;
-        printf("%c ", input[i]);
+            if(input[i] == 32 + k) {
+                input[i] = 32;
+            }
+
+        printf("%c", input[i]);
     }
 
 return 0;
